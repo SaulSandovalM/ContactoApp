ef2b91678d0ed5a579684723664dabd3d8f93472
SaulSandovalM <saul_watch_dogs@outlook.com>
2016-11-05 14:58:41 -0600

    Primer comit

---
+++ app/src/main/java/com/example/root/sedeco/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.root.sedeco;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
